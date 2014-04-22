963c3505a6298ce55775a79b778dd31b3fe6ff06
franticdeli <mark.schlottke@gmail.com>
2014-04-22 15:35:58 -0500

    Fixed some things

--- Homework7/src/HuffmanTree.java
+++ Homework7/src/HuffmanTree.java
@@ -1,10 +1,8 @@
 import java.io.*;
 import java.util.PriorityQueue;
 
-class HuffmanNode
-    implements Comparable
+class HuffmanNode implements Comparable<HuffmanNode>
 {
-
   private char label;
   private int weight;
   private HuffmanNode leftChild;
@@ -24,10 +22,12 @@ class HuffmanNode
     this.label = c;
     this.weight = freq;
   }
+
   public boolean isRoot()
   {
     return isRoot;
   }
+
   public char getLabel()
   {
     return label;
@@ -68,12 +68,6 @@ class HuffmanNode
     this.leftChild = leftChild;
   }
 
-  @Override
-  public int compareTo(Object object)
-  {
-    return compareTo((HuffmanNode) object);
-  }
-
   public int compareTo(HuffmanNode hn)
   {
     return this.weight - hn.getWeight();
@@ -91,39 +85,38 @@ public class HuffmanTree
     //use printCodes helper method
     printCodes(root, "");
   }
-  
+
   private void printCodes(HuffmanNode node, String string)
   {
-    if(!node.isRoot())
-      {
-        //If not a root/parent node print out the label and generated Huffman Code
-        System.out.println(node.getLabel() +": "+ string);
-      }
-    else
-      {
-        //For left child add "0" to string 
-        String lString = string.concat("0");
-        printCodes(node.getLeftChild(), lString);
-        
-        //For right child add "1" to string
-        String rString = string.concat("1");
-        printCodes(node.getRightChild(), rString);
-      }
+    if (!node.isRoot())
+    {
+      //If not a root/parent node print out the label and generated Huffman Code
+      System.out.println(node.getLabel() + ": " + string);
+    } else
+    {
+      //For left child add "0" to string 
+      String lString = string.concat("0");
+      printCodes(node.getLeftChild(), lString);
+
+      //For right child add "1" to string
+      String rString = string.concat("1");
+      printCodes(node.getRightChild(), rString);
+    }
   }
 
   private void buildHuffmanTree()
   {
-    HuffmanNode pn = null;
-    HuffmanNode pn2 = null;
+    HuffmanNode hn = null;
+    HuffmanNode hn2 = null;
 
     while (myQueue.size() > 1)
-      {
-        //Take top two nodes off the PQ
-        HuffmanNode hn = myQueue.poll();
-        HuffmanNode hn2 = myQueue.poll();
-        //Combine weights into new parent/root node and add back to PQ
-        myQueue.add(new HuffmanNode(hn.getWeight() + hn2.getWeight(), hn, hn2));
-      }
+    {
+      //Take top two nodes off the PQ
+      hn = myQueue.poll();
+      hn2 = myQueue.poll();
+      //Combine weights into new parent/root node and add back to PQ
+      myQueue.add(new HuffmanNode(hn.getWeight() + hn2.getWeight(), hn, hn2));
+    }
     root = myQueue.poll();
 
   }
@@ -133,45 +126,38 @@ public class HuffmanTree
     myQueue = new PriorityQueue<>();
     BufferedReader br;
     try
+    {
+      FileReader fr = new FileReader(string);
+      br = new BufferedReader(fr);
+      for (String line; (line = br.readLine()) != null;)
       {
-        FileReader fr = new FileReader(string);
-        br = new BufferedReader(fr);
-        for (String line; (line = br.readLine()) != null;)
-          {
-            char fc = line.charAt(0);
-            if (line.charAt(1) == ':')
-              {
-                String freq = line.substring(2, line.length());
-                HuffmanNode hn = new HuffmanNode(fc, Integer.parseInt(freq));
-                myQueue.add(hn);
-              }
-          }
-
-      }
-    catch (Exception e)
-      {
-        e.printStackTrace();
+        char fc = line.charAt(0);
+        if (line.charAt(1) == ':')
+        {
+          String freq = line.substring(2, line.length());
+          HuffmanNode hn = new HuffmanNode(fc, Integer.parseInt(freq));
+          myQueue.add(hn);
+        }
       }
 
-//    for (HuffmanNode h : myQueue)
-//      {
-//        System.out.println(h.getLabel() + ":" + h.getWeight());
-//      }
-
+    } catch (Exception e)
+    {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String args[]) throws FileNotFoundException
   {
     if (args.length != 1)
-      {
-        System.out.println("Usage: HuffmanTree <inputfilename>");
-        System.exit(0);
-      }
+    {
+      System.out.println("Usage: HuffmanTree <inputfilename>");
+      System.exit(0);
+    }
     HuffmanTree tree = new HuffmanTree();
     tree.initQueue(args[0]);
     tree.buildHuffmanTree();
-    System.out.println("The Huffman codewords for the alphabet and weights specified in "
-                       + args[0] + " is:");
+    System.out.println("The Huffman codewords for the alphabet and weights specified in " + args[0]
+        + " is:");
     tree.printCodewords();
   }
 
